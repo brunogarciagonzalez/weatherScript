0e3d360c1a03f9537b6c6085f376e9fa74247970
rpyoungdale <youngdale95@gmail.com>
2018-05-28 12:41:47 -0400

    create new account now functional

--- src/components/CityTile.js
+++ src/components/CityTile.js
@@ -38,39 +38,35 @@ class CityTile extends React.Component {
 
   render() {
     return (
-      <div>
+      <div className="column">
         {this.state.currentDay ? (
-          <div className="column">
-            <div className="ui fluid link card">
-              <div className="image">
-                <img
-                  src={require(`../weather_images/${
-                    this.state.currentDay.weather_state_abbr
-                  }.svg`)}
-                />
+          <div className="ui link card">
+            <div className="image">
+              <img
+                src={require(`../weather_images/${
+                  this.state.currentDay.weather_state_abbr
+                }.svg`)}
+              />
+            </div>
+            <div className="content">
+              <div className="header">{this.state.cityName}</div>
+              <div className="meta">
+                <span className="date">
+                  {new Date(this.state.currentDay.applicable_date)
+                    .toDateString()
+                    .split(" ")
+                    .slice(1)
+                    .join(" ")}
+                </span>
               </div>
-              <div className="content">
-                <div className="header">{this.state.cityName}</div>
-                <div className="meta">
-                  <span className="date">
-                    {new Date(this.state.currentDay.applicable_date)
-                      .toDateString()
-                      .split(" ")
-                      .slice(1)
-                      .join(" ")}
-                  </span>
-                </div>
-                <div className="description">
-                  Current Temp:{" "}
-                  {this.celsiusConversion(this.state.currentDay.the_temp)}
-                  <br />
-                  Hi / Low:{" "}
-                  {`${this.celsiusConversion(
-                    this.state.currentDay.max_temp
-                  )} / ${this.celsiusConversion(
-                    this.state.currentDay.min_temp
-                  )}`}
-                </div>
+              <div className="description">
+                Current Temp:{" "}
+                {this.celsiusConversion(this.state.currentDay.the_temp)}
+                <br />
+                Hi / Low:{" "}
+                {`${this.celsiusConversion(
+                  this.state.currentDay.max_temp
+                )} / ${this.celsiusConversion(this.state.currentDay.min_temp)}`}
               </div>
             </div>
           </div>
